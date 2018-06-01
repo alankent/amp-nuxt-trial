27837d9531630920d0953bf4707b6f56db12ca44
Alan Kent <alan.james.kent@gmail.com>
2018-05-31 18:00:27 -0700

    Pulled out Header element.

--- pages/main.vue
+++ pages/main.vue
@@ -1,65 +1,6 @@
 <template>
 <div>
 <!--
-<!doctype html>
-<html ⚡>
-  <head>
-    <meta charset="utf-8">
-    <title>Sample document</title>
-    <link rel="canonical" href="./regular-html-version.html">
-    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-
-    <style amp-custom>
-
-      .headerbar {
-        padding-left: 1rem;
-        padding-right: 1rem;
-        padding-top: 0;
-        padding-bottom: 0;
-        background-color: #fff;
-        height: 3rem;
-        box-shadow: none;
-        border-bottom: 1px solid #f3f3f3;
-        left: 0;
-        right: 0;
-        top: calc(0px);
-        position: fixed;
-        justify-content: flex-start;
-        -webkit-box-align: center;
-        align-items: center;
-        display: flex;
-        z-index: 999;
-      }
-
-      .headerbar-icon-wrapper {
-        width: 25px;
-        position: relative;
-      }
-
-      .headerbar-fixed-link {
-        margin-right: 0;
-        text-decoration: none;
-        background-color: transparent;
-      }
-
-      .headerbar-title {
-        width: 100%;
-        text-align: center;
-      }
-
-      body {
-        font-family: Helvetica, sans-serif;
-      }
-
-      main {
-        max-width: 412px;
-        margin: auto;
-      }
-      
-      p {
-        margin-top: 0.5rem;
-        margin-bottom: 0.5rem;
-      }
 
       .field-label {
         text-size: 80%;
@@ -127,38 +68,9 @@
     <script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script>
     <script async custom-element="amp-list" src="https://cdn.ampproject.org/v0/amp-list-0.1.js"></script>
 
-    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
-    <script async src="https://cdn.ampproject.org/v0.js"></script>
-  </head>
-  <body>
-
 -->
 
-    <!-- TODO: FROM BIKE SHED -->
-    <header class="headerbar">
-
-      <!-- Hamburger menu -->
-      <div role="button" aria-label="open sidebar" on="tap:header-sidebar.toggle" tabindex="0" class="ampstart-navbar-trigger">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg>
-      </div>
-
-      <div class="headerbar-title">
-        <a href="landing.amp.html">
-          <!--<amp-img src="../../img/e-commerce/logo.png" width="96" height="27" layout="fixed" class="my0 mx-auto" alt=""></amp-img>-->
-          <span class="headerbar-title">Tour Tickets</span>
-        </a>
-      </div>
-
-      <!-- Cart icon -->
-      <div class="headerbar-fixed">
-	<a href="cart.amp.html" class="headerbar-fixed-link">
-	  <div class="headerbar-icon-wrapper">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 37 35"><g><path d="M.8.47598c-.4 0-.8.3-.8.8s.4.9.8.9h4.4l4.4 21.6c0 .5.4 1 .9.9h21c.4 0 .9-.4.9-.9s-.4-.9-.9-.9H11.2l-.7-3.5h22.7c.4 0 .7-.3.8-.7l2.9-13c.1-.5-.3-1.1-.8-1.1H7.5l-.8-3.5c-.1-.3-.4-.6-.8-.6H.8zm7 6h27.3l-2.6 11.3H10.1l-2.3-11.3zm6.9 19.9c-2.1 0-3.8 1.8-3.8 3.9s1.7 3.9 3.8 3.9c2.1 0 3.8-1.8 3.8-3.9 0-2.1-1.7-3.9-3.8-3.9zm12.6 0c-2.1 0-3.8 1.8-3.8 3.9s1.7 3.9 3.8 3.9c2.1 0 3.8-1.8 3.8-3.9 0-2.1-1.7-3.9-3.8-3.9zm-12.6 1.7c1.2 0 2.1 1 2.1 2.2 0 1.2-.9 2.2-2.1 2.2-1.2 0-2.1-1-2.1-2.2 0-1.2.9-2.2 2.1-2.2zm12.6 0c1.2 0 2.1 1 2.1 2.2 0 1.2-.9 2.2-2.1 2.2-1.2 0-2.1-1-2.1-2.2 0-1.2 1-2.2 2.1-2.2z" fill="#222"></path></g></svg>
-          </div>
-	</a>
-      </div>
-
-    </header>
+    <Header/>
 
     <!-- Parallax scrolled hero image -->
     <HeroImage/>
@@ -251,11 +163,6 @@
       <p>Some text</p>
       <p>Some text</p>
       <p>Some text</p>
-      <!-- FAILED TEST
-      <amp-iframe layout="responsive" width="4" height="3" src="https://poly.google.com/view/5cp769JNOCe/embed?chrome=min&amp=" frameborder="0" allowvr="yes" allow="vr; xr; accelerometer; magnetometer; gyroscope;" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel="" ></amp-iframe>
-      <p>Some text</p>
-      <iframe width="100%" height="100%" src="https://poly.google.com/view/5cp769JNOCe/embed?chrome=min" frameborder="0" style="border:none;" allowvr="yes" allow="vr; xr; accelerometer; magnetometer; gyroscope;" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel="" ></iframe>
-      -->
       <p>Some text</p>
       <p>Some text</p>
       <p>Some text</p>
@@ -293,32 +200,37 @@
     </main>
 
     <Footer/>
-</div>
-<!--
-  </body>
-</html>
--->
-
+  </div>
 </template>
 
 <script>
 import HeroImage from '~/components/HeroImage.vue'
+import Header from '~/components/Header.vue'
 import Footer from '~/components/Footer.vue'
 
 export default {
   components: {
     HeroImage,
+    Header,
     Footer,
   },
-  head () {
-    return {
-    script: [
-      { innherHTML: 'console.log("hello")', type: 'text/javascript', charset: 'utf-8' },
-      { async: 'async', 'custom-element': "amp-animation", src: "https://cdn.ampproject.org/v0/amp-animation-0.1.js" },
-      { async: 'async', 'custom-element': "amp-position-observer", src: "https://cdn.ampproject.org/v0/amp-position-observer-0.1.js" },
-    ]
-    };
-  }
 }
 </script>
 
+<style>
+
+body {
+  font-family: Helvetica, sans-serif;
+}
+
+main {
+  max-width: 412px;
+  margin: auto;
+}
+
+p {
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+
+</style>
